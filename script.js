678d82c6574c00b836868d4b64b94d462d39d7f4
Dr Phil <philip.prewitt@gmail.com>
2020-07-11 09:52:44 -0600

    fixing writePassword to start the process

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
-var confirmLength = prompt("How many characters would you like for your password? (8-128 characters)");
+
+//define writePassword
+function writePassword() {
+  
+  var confirmLength = prompt("How many characters would you like for your password? (8-128 characters)");
   var confirmLowercase = confirm("Would you like to include lower case letters?");
   var confirmSymbols = confirm("Would you like to include symbols?");
   var confirmNumbers = confirm("Would you like to include numbers?");
   var confirmUppercase = confirm("Would you like to include upper case letters?");
 
- //length value 
+ //define length value 
   var lengthValue = parseInt(confirmLength);
   
-
 // get random lower case letter function
 function getRandomLowerCase() {
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
@@ -49,15 +54,37 @@ function getRandomLowerCase() {
       if (confirmLowercase === true) {
         userEntry.push(getRandomLowerCase);
       }
-      
+      if (
+            
+        confirmSymbols === false &&
+        confirmUppercase === false &&
+        confirmLowercase === false &&
+        confirmNumbers === false
+      ) {
+        alert(
+          "You must select at least one character type. Please refresh and select one."
+        );
+      }
+    } else {
+      alert("Please choose a password with a length of 8-128. Refresh and try again.");
+    }
+    var pass = "";
+    for (var i = 0; i < lengthValue; i++) {
+      var newRandom = Math.floor(Math.random() * userEntry.length);
+      pass = pass.concat(userEntry[newRandom]());
+    }
+    return pass;
+    alert(pass);
+  }
+  
   // Assignment Code
   var generateBtn = document.querySelector("#generate");
   
-  function writePassword() {
+  
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
   
     passwordText.value = password;
   }
-  
+
   
