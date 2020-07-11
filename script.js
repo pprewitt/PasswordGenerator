89976cdc4860506e16cbf84602e13a68862c0082
Dr Phil <philip.prewitt@gmail.com>
2020-07-10 21:16:34 -0600

    fixing random symbol function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
-var confirmLength = prompt(
-    "How many characters would you like for your password? (8-128 characters)"
-  );
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+var confirmLength = prompt("How many characters would you like for your password? (8-128 characters)");
   var confirmLowercase = confirm("Would you like to include lower case letters?");
   var confirmSymbols = confirm("Would you like to include symbols?");
   var confirmNumbers = confirm("Would you like to include numbers?");
   var confirmUppercase = confirm("Would you like to include upper case letters?");
 
  //length value 
-  var lengthValue = parseInt(confirmLength)
+  var lengthValue = parseInt(confirmLength);
   
+
 // get random lower case letter function
 function getRandomLowerCase() {
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
@@ -28,22 +29,35 @@ function getRandomLowerCase() {
   
   //get random symbol function
   function getRandomSymbol() {
-    const symbols = "!#$%&()*+,-./:;<=>?@";
-    return symbols [Math.floor(Math.random * symbols.length)];
+    return String.fromCharCode(Math.floor(Math.random()*15) + 33);
 
   }
 
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  function generatePassword() {
+    if (lengthValue >= 8 && lengthValue <= 128) {
+      var userEntry = [];
+  
+      if (confirmSymbols === true) {
+        userEntry.push(getRandomSymbol);
+      }
+      if (confirmUppercase === true) {
+        userEntry.push(getRandomUpperCase);
+      }
+      if (confirmNumbers === true) {
+        userEntry.push(getRandomNumber);
+      }
+      if (confirmLowercase === true) {
+        userEntry.push(getRandomLowerCase);
+      }
+      
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
+  
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+  
